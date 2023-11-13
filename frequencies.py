b9516cf4bff3fd0df42177af4adb626e6807aad7
Yiqing Ren <yiqing.1.ren@kcl.ac.uk>
2023-11-13 17:51:27 +0000

    Count the frequencies

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,10 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        key = item if isinstance (item, str) else str(item)
+        if key in frequencies:
+            frequencies[key] += 1
+        else:
+            frequencies[key] = 1
     return frequencies
